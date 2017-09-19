ba94a6d0081054b645cea4e3b104eb13772669e7
Jen <Jennifer.Hiar@allstate.com>
2017-09-19 11:15:10 -0500

    commit

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'mw-media-item',
@@ -7,8 +8,9 @@ import { Component, Input } from '@angular/core';
 })
 export class MediaItemComponent {
   @Input() mediaItem;
-
+  @Output() delete = new EventEmitter();
+  
   onDelete() {
-    console.log('deleted');
+    this.delete.emit(this.mediaItem);
   }
 }
